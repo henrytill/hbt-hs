7ad2098574343b62ceb15210f98a343e42d5e76d
Henry Till <henrytill@gmail.com>
2025-07-07 17:06:13 -0700

    Hbt.Markdown: prepare to improve error handling

--- src/Hbt/Markdown.hs
+++ src/Hbt/Markdown.hs
@@ -6,7 +6,8 @@ import Commonmark (ParseError, commonmarkWith, defaultSyntaxSpec)
 import Commonmark.Initial (Block, BlockF (..), Blocks, Inline, InlineF (..))
 import Control.Comonad.Cofree
 import Data.Bifunctor
-import Data.Foldable (foldl')
+import Data.Foldable (foldlM)
+import Data.Functor ((<&>))
 import Data.Functor.Identity (runIdentity)
 import Data.Maybe qualified as Maybe
 import Data.Text (Text)
@@ -24,13 +25,14 @@ data MarkdownError
 
 type Acc = (Collection, FoldState)
 
-saveEntity :: Acc -> Acc
+saveEntity :: Acc -> Either MarkdownError Acc
 saveEntity (c, st) =
-  ( foldr (Collection.addEdges entity.uri) (Collection.upsert entity c) (Maybe.listToMaybe st.parents)
-  , st {uri = Nothing, name = Nothing, maybeParent = Just entity.uri}
-  )
+  return
+    ( foldr (Collection.addEdges entity.uri) (Collection.upsert entity c) (Maybe.listToMaybe st.parents) -- Collection.addEdges throws
+    , st {uri = Nothing, name = Nothing, maybeParent = Just entity.uri}
+    )
   where
-    entity = Maybe.fromJust (FoldState.toEntity st) -- throws
+    entity = Maybe.fromJust (FoldState.toEntity st) -- Maybe.fromJust throws
 
 textFromInlines :: [Inline a] -> Text
 textFromInlines = foldMap go
@@ -49,47 +51,47 @@ textFromInlines = foldMap go
       Code t -> "`" <> t <> "`"
       RawInline _ t -> t
 
-extractLink :: Text -> Text -> [Inline a] -> Acc -> Acc
-extractLink d _ desc (c, st) = (c, st {name, uri})
+extractLink :: Text -> Text -> [Inline a] -> Acc -> Either MarkdownError Acc
+extractLink d _ desc (c, st) = return (c, st {name, uri})
   where
-    uri = Just . mkURI $ Text.unpack d -- throws
+    uri = Just . mkURI $ Text.unpack d -- mkURI throws
     linkText = textFromInlines desc
     name
       | linkText == d = Nothing
       | Text.null linkText = Nothing
       | otherwise = Just $ MkName linkText
 
-inlineFolder :: Acc -> Inline a -> Acc
+inlineFolder :: Acc -> Inline a -> Either MarkdownError Acc
 inlineFolder acc (_ :< il) = case il of
-  Link d t desc -> saveEntity $ extractLink d t desc acc
-  _ -> acc
+  Link d t desc -> extractLink d t desc acc >>= saveEntity
+  _ -> return acc
 
-blockFolder :: Acc -> Block a -> Acc
+blockFolder :: Acc -> Block a -> Either MarkdownError Acc
 blockFolder acc@(c, st) (_ :< b) = case b of
   Plain ils ->
-    foldl' inlineFolder acc ils
+    foldlM inlineFolder acc ils
   Heading 1 ils ->
-    (c, st {time, maybeParent = Nothing, labels = []})
+    return (c, st {time, maybeParent = Nothing, labels = []})
     where
       headingText = textFromInlines ils
-      time = Just . mkTime $ Text.unpack headingText -- throws
+      time = Just . mkTime $ Text.unpack headingText -- mkTime throws
   Heading level ils ->
-    (c, st {labels})
+    return (c, st {labels})
     where
       headingText = textFromInlines ils
       labels = MkLabel headingText : take (level - 2) st.labels
   List _ _ bss ->
-    f <$> foldl' (foldl' blockFolder) acc' bss
+    foldlM (foldlM blockFolder) acc' bss <&> fmap f
     where
       acc' = (c, foldr (\parent s -> s {parents = parent : s.parents}) st st.maybeParent)
       f s = s {maybeParent = Nothing, parents = drop 1 s.parents}
-  _ -> acc
+  _ -> return acc
 
-collectionFromBlocks :: [Block a] -> Collection
-collectionFromBlocks = fst . foldl' blockFolder (Collection.empty, FoldState.empty)
+collectionFromBlocks :: [Block a] -> Either MarkdownError Collection
+collectionFromBlocks bs = foldlM blockFolder (Collection.empty, FoldState.empty) bs <&> fst
 
 parseBlocks :: String -> Text -> Either ParseError Blocks
 parseBlocks name = runIdentity . commonmarkWith defaultSyntaxSpec name
 
 parse :: String -> Text -> Either MarkdownError Collection
-parse name input = collectionFromBlocks <$> first CommonmarkError (parseBlocks name input)
+parse name input = first CommonmarkError (parseBlocks name input) >>= collectionFromBlocks
