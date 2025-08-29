ee2c54216f9b1ff878a8d6594f275a1a3a6381fc
Henry Till <henrytill@gmail.com>
2025-08-29 04:45:24 -0700

    Hbt.Formatter.HTML: revamp

--- src/Hbt/Formatter/HTML.hs
+++ src/Hbt/Formatter/HTML.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Hbt.Formatter.HTML
@@ -5,59 +6,68 @@ module Hbt.Formatter.HTML
   )
 where
 
-import Data.Aeson (Value (..), object, (.=))
+import Data.Aeson (ToJSON (..), object, (.=))
 import Data.List (sort)
-import Data.Maybe (catMaybes)
+import Data.Maybe (listToMaybe)
 import Data.Set qualified as Set
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.Lazy qualified as LText
 import Data.Vector qualified as Vector
+import GHC.Generics (Generic)
 import Hbt.Collection (Collection)
 import Hbt.Collection qualified as Collection
 import Hbt.Collection.Entity (Entity (..), Extended (..), Label (..), Name (..), Time (..), URI (..))
 import Text.Microstache (Template, renderMustache)
 
--- | Format a Collection as an HTML string in Netscape Bookmark format
-format :: Template -> Collection -> Text
-format template collection = LText.toStrict $ renderMustache template templateData
-  where
-    templateData = object ["entities" .= map entityToJSON (Vector.toList $ Collection.allEntities collection)]
+data TemplateEntity = MkTemplateEntity
+  { uri :: Text
+  , createdAt :: Text
+  , title :: Text
+  , lastModified :: Maybe Text
+  , tags :: Maybe Text
+  , shared :: Bool
+  , toRead :: Bool
+  , isFeed :: Bool
+  , lastVisit :: Maybe Text
+  , description :: Maybe Text
+  }
+  deriving (Generic)
 
--- | Convert an Entity to JSON for template rendering
-entityToJSON :: Entity -> Value
-entityToJSON entity@(MkEntity uri createdAt _ names labels shared toRead isFeed extended lastVisitedAt) =
-  object $
-    catMaybes
-      [ Just ("uri" .= uriText)
-      , Just ("createdAt" .= formatTime createdAt)
-      , Just ("title" .= getFirstName names)
-      , fmap (\t -> "lastModified" .= formatTime t) (getLastModified entity)
-      , if null tagsList then Nothing else Just ("tags" .= tagsText)
-      , Just ("shared" .= shared)
-      , Just ("toRead" .= toRead)
-      , Just ("isFeed" .= isFeed)
-      , fmap (\t -> "lastVisit" .= formatTime t) lastVisitedAt
-      , fmap (\(MkExtended desc) -> "description" .= desc) extended
-      ]
+instance ToJSON TemplateEntity
+
+format :: Template -> Collection -> Text
+format template collection =
+  LText.toStrict . renderMustache template $
+    object ["entities" .= map toTemplateEntity (Vector.toList $ Collection.allEntities collection)]
+
+toTemplateEntity :: Entity -> TemplateEntity
+toTemplateEntity entity =
+  MkTemplateEntity
+    { uri = uriText
+    , createdAt = formatTime entity.createdAt
+    , title = getFirstName uriText entity.names
+    , lastModified = fmap formatTime (getLastModified entity)
+    , tags = if null tagsList then Nothing else Just tagsText
+    , shared = entity.shared
+    , toRead = entity.toRead
+    , isFeed = entity.isFeed
+    , lastVisit = fmap formatTime entity.lastVisitedAt
+    , description = fmap (.unExtended) entity.extended
+    }
   where
-    MkURI uriVal = uri
+    MkURI uriVal = entity.uri
     uriText = Text.pack $ show uriVal
-    tagsList = sort $ map (\(MkLabel lbl) -> lbl) $ Set.toList labels
+    tagsList = sort . map (.unLabel) $ Set.toList entity.labels
     tagsText = Text.intercalate "," tagsList
 
--- | Get the first name alphabetically, or use the URI if no names
-getFirstName :: Set.Set Name -> Text
-getFirstName names
-  | Set.null names = ""
-  | otherwise = let (MkName name) = Set.findMin names in name
+getFirstName :: Text -> Set.Set Name -> Text
+getFirstName def names
+  | Set.null names = def
+  | otherwise = (.unName) $ Set.findMin names
 
--- | Get the last modified time (first in updatedAt list)
 getLastModified :: Entity -> Maybe Time
-getLastModified (MkEntity _ _ updatedAt _ _ _ _ _ _ _) = case updatedAt of
-  [] -> Nothing
-  (t : _) -> Just t
+getLastModified = listToMaybe . (.updatedAt)
 
--- | Format a time as Unix timestamp
 formatTime :: Time -> Text
-formatTime (MkTime posixTime) = Text.pack $ show (round posixTime :: Integer)
+formatTime (MkTime posixTime) = Text.pack . show @Integer $ round posixTime
