7b2c72afab9d4cce0fdc59819da9107016df67c7
Henry Till <henrytill@gmail.com>
2025-08-31 00:20:54 -0700

    test: improve deriveAllConstructors tests

--- test/Hbt/Base/THTest.hs
+++ test/Hbt/Base/THTest.hs
@@ -1,50 +1,71 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeData #-}
+
 module Hbt.Base.THTest where
 
 import Hbt.Base.TH
-import Language.Haskell.TH
 import Test.Dwergaz
 import TestUtilities (testResults)
 
-extractorTests :: Test
-extractorTests =
-  group
-    "Constructor extraction"
-    [ assertEqual "extractEnumConstructorNames extracts from normal constructors" 2 $
-        let constructors = [NormalC (mkName "From") [], NormalC (mkName "To") []]
-            names = extractEnumConstructorNames constructors
-         in length names
-    , assertEqual "extractEnumConstructorNames extracts from GADT constructors" 1 $
-        let constructors = [GadtC [mkName "From"] [] (ConT (mkName "Flow"))]
-            names = extractEnumConstructorNames constructors
-         in length names
-    ]
+data RegularFlow = RegularFrom | RegularTo
+  deriving (Eq, Show)
 
-validationTests :: Test
-validationTests =
+data RegularFormat (f :: RegularFlow) where
+  RegularJSON :: RegularFormat RegularFrom
+  RegularXML :: RegularFormat RegularFrom
+  RegularHTML :: RegularFormat f
+  RegularYAML :: RegularFormat RegularTo
+
+deriving instance Eq (RegularFormat f)
+
+deriving instance Show (RegularFormat f)
+
+$(deriveAllConstructors ''RegularFormat ''RegularFlow)
+
+type data TypeDataFlow = TypeDataFrom | TypeDataTo
+
+data TypeDataFormat (f :: TypeDataFlow) where
+  TypeDataJSON :: TypeDataFormat TypeDataFrom
+  TypeDataXML :: TypeDataFormat TypeDataFrom
+  TypeDataHTML :: TypeDataFormat f
+  TypeDataYAML :: TypeDataFormat TypeDataTo
+
+deriving instance Eq (TypeDataFormat f)
+
+deriving instance Show (TypeDataFormat f)
+
+$(deriveAllConstructors ''TypeDataFormat ''TypeDataFlow)
+
+constructorGenerationTests :: Test
+constructorGenerationTests =
   group
-    "Type validation"
-    [ assertBool "isValidForEnum matches exact types" $
-        let resultType = AppT (ConT (mkName "Format")) (ConT (mkName "From"))
-            targetType = ConT (mkName "From")
-         in isValidForEnum resultType targetType
-    , assertBool "isValidForEnum matches polymorphic types" $
-        let resultType = AppT (ConT (mkName "Format")) (VarT (mkName "f"))
-            targetType = ConT (mkName "From")
-         in isValidForEnum resultType targetType
-    , assertBool "isValidForEnum rejects mismatched types" $
-        let resultType = AppT (ConT (mkName "Format")) (ConT (mkName "To"))
-            targetType = ConT (mkName "From")
-         in not $ isValidForEnum resultType targetType
+    "Constructor generation"
+    [ assertEqual
+        "Regular data: exact RegularFrom constructors"
+        [RegularJSON, RegularXML, RegularHTML]
+        allRegularFromConstructors
+    , assertEqual
+        "Regular data: exact RegularTo constructors"
+        [RegularHTML, RegularYAML]
+        allRegularToConstructors
+    , assertEqual
+        "Type data: exact TypeDataFrom constructors"
+        [TypeDataJSON, TypeDataXML, TypeDataHTML]
+        allTypeDataFromConstructors
+    , assertEqual
+        "Type data: exact TypeDataTo constructors"
+        [TypeDataHTML, TypeDataYAML]
+        allTypeDataToConstructors
     ]
 
 allTests :: Test
 allTests =
   group
-    "Hbt.TH"
-    [ extractorTests
-    , validationTests
+    "Hbt.Base.TH"
+    [ constructorGenerationTests
     ]
 
--- Run tests
 results :: (String, Bool)
-results = testResults "Hbt.TH" allTests
+results = testResults "Hbt.Base.TH" allTests
