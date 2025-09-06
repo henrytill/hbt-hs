ec665e61202712ae00b2dce3509984e142e7263a
Henry Till <henrytill@gmail.com>
2025-09-06 11:34:21 -0700

    replace tagsoup with xeno

--- src/Hbt/Parser/Common.hs
+++ src/Hbt/Parser/Common.hs
@@ -7,6 +7,9 @@ module Hbt.Parser.Common
     IsNull (..)
   , pattern Null
 
+    -- * Attribute
+  , Attribute
+
     -- * Attribute patterns
   , pattern Href
   , pattern Description
@@ -41,16 +44,22 @@ where
 
 import Control.Monad.State.Strict (StateT)
 import Control.Monad.State.Strict qualified as State
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as ByteString
+import Data.ByteString.Char8 qualified as Char8
 import Data.Text (Text)
 import Data.Text qualified as Text
+import Data.Text.Encoding qualified as Text
 import Hbt.Entity (URI (..), nullURI)
-import Text.HTML.TagSoup (Attribute)
 
 class IsNull s where
   isNull :: s -> Bool
 
 instance IsNull Text where
   isNull = Text.null
+
+instance IsNull ByteString where
+  isNull = ByteString.null
 
 instance IsNull URI where
   isNull uri = uri == nullURI
@@ -60,40 +69,42 @@ pattern Null <- (isNull -> True)
   where
     Null = mempty
 
-pattern Href :: Text -> Attribute Text
+type Attribute = (ByteString, ByteString)
+
+pattern Href :: Text -> Attribute
 pattern Href value <- (matchAttr "href" -> Just value)
 
-pattern Description :: Text -> Attribute Text
+pattern Description :: Text -> Attribute
 pattern Description value <- (matchAttr "description" -> Just value)
 
-pattern Extended :: Text -> Attribute Text
+pattern Extended :: Text -> Attribute
 pattern Extended value <- (matchAttr "extended" -> Just value)
 
-pattern Time :: Text -> Attribute Text
+pattern Time :: Text -> Attribute
 pattern Time value <- (matchAttr "time" -> Just value)
 
-pattern AddDate :: Text -> Attribute Text
+pattern AddDate :: Text -> Attribute
 pattern AddDate value <- (matchAttr "add_date" -> Just value)
 
-pattern LastModified :: Text -> Attribute Text
+pattern LastModified :: Text -> Attribute
 pattern LastModified value <- (matchAttr "last_modified" -> Just value)
 
-pattern LastVisit :: Text -> Attribute Text
+pattern LastVisit :: Text -> Attribute
 pattern LastVisit value <- (matchAttr "last_visit" -> Just value)
 
-pattern Private :: Text -> Attribute Text
+pattern Private :: Text -> Attribute
 pattern Private value <- (matchAttr "private" -> Just value)
 
-pattern Shared :: Text -> Attribute Text
+pattern Shared :: Text -> Attribute
 pattern Shared value <- (matchAttr "shared" -> Just value)
 
-pattern ToRead :: Text -> Attribute Text
+pattern ToRead :: Text -> Attribute
 pattern ToRead value <- (matchAttr "toread" -> Just value)
 
-pattern Feed :: Text -> Attribute Text
+pattern Feed :: Text -> Attribute
 pattern Feed value <- (matchAttr "feed" -> Just value)
 
-pattern Tags :: [Text] -> Attribute Text
+pattern Tags :: [Text] -> Attribute
 pattern Tags values <- (matchAttrTagList -> Just values)
 
 pattern One :: Text
@@ -105,21 +116,27 @@ pattern STrue = "true"
 pattern Yes :: Text
 pattern Yes = "yes"
 
-matchAttr :: Text -> Attribute Text -> Maybe Text
+toLower :: ByteString -> ByteString
+-- toLower bs = Char8.map Char.toLower bs
+toLower bs = bs
+
+matchAttr :: ByteString -> Attribute -> Maybe Text
 matchAttr key (attrKey, attrValue)
-  | Text.toLower attrKey == Text.toLower key = if isNull attrValue then Nothing else Just attrValue
+  | toLower attrKey == toLower key
+  , not (isNull attrValue) =
+      Just (Text.decodeUtf8 attrValue)
   | otherwise = Nothing
 
-parseTagStringWith :: Text -> Text -> [Text]
+parseTagStringWith :: ByteString -> ByteString -> [Text]
 parseTagStringWith separator value
   | isNull value = []
-  | otherwise = filter (not . isNull) (map Text.strip (Text.splitOn separator value))
+  | otherwise = filter (not . isNull) (map (Text.decodeUtf8 . Char8.strip) (Char8.split (Char8.head separator) value))
 
 -- (Pinboard) XML format uses "tag" with space-separated values: tag="web programming haskell"
 -- HTML format uses "tags" with comma-separated values: TAGS="web,programming,haskell"
-matchAttrTagList :: Attribute Text -> Maybe [Text]
+matchAttrTagList :: Attribute -> Maybe [Text]
 matchAttrTagList (attrKey, attrValue) =
-  case Text.toLower attrKey of
+  case toLower attrKey of
     "tag" -> Just (parseTagStringWith " " attrValue)
     "tags" -> Just (parseTagStringWith "," attrValue)
     _ -> Nothing
--- src/Hbt/Parser/Pinboard/XML.hs
+++ src/Hbt/Parser/Pinboard/XML.hs
@@ -6,6 +6,7 @@ import Control.Monad.Except (MonadError)
 import Control.Monad.Except qualified as Except
 import Data.Text (Text)
 import Data.Text qualified as Text
+import Data.Text.Encoding qualified as Text
 import Hbt.Collection (Collection)
 import Hbt.Collection qualified as Collection
 import Hbt.Entity (Entity)
@@ -14,15 +15,16 @@ import Hbt.Parser.Common
 import Hbt.Parser.Pinboard.Common
 import Lens.Family2
 import Lens.Family2.State.Strict
-import Text.HTML.TagSoup (Attribute, Tag (..))
-import Text.HTML.TagSoup qualified as TagSoup
 import URI.ByteString (URIParseError)
+import Xeno.DOM qualified as Xeno
+import Xeno.Types (XenoException)
 
 data Error
   = EntityInvalidURI URIParseError
   | EntityInvalidTime Text
   | ParseError String
-  deriving (Show, Eq)
+  | XenoError XenoException
+  deriving (Show)
 
 fromEntityError :: Entity.Error -> Error
 fromEntityError (Entity.InvalidURI s) = EntityInvalidURI s
@@ -53,7 +55,7 @@ newtype PinboardM a = MkPinboardM (ParserMonad ParseState Error a)
 runPinboardM :: PinboardM a -> ParseState -> Either Error (a, ParseState)
 runPinboardM (MkPinboardM m) = runParserMonad m
 
-accumulatePostAttr :: PinboardPost -> Attribute Text -> PinboardPost
+accumulatePostAttr :: PinboardPost -> Attribute -> PinboardPost
 accumulatePostAttr post attr =
   case attr of
     Href href -> post {href}
@@ -65,29 +67,43 @@ accumulatePostAttr post attr =
     ToRead Yes -> post {toread = Just PinboardTrue}
     _ -> post
 
-createPostFromAttrs :: [Attribute Text] -> Either Error PinboardPost
+createPostFromAttrs :: [Attribute] -> Either Error PinboardPost
 createPostFromAttrs attrs = do
   let accumulated = foldl' accumulatePostAttr emptyPinboardPost attrs
   if isNull accumulated.href
     then Left (ParseError "missing required attribute: href")
     else pure accumulated
 
-handle :: Tag Text -> PinboardM ()
-handle (TagOpen "post" attrs) = do
-  post <- Except.liftEither (createPostFromAttrs attrs)
-  result <- Except.liftEither (postToEntity fromEntityError post)
-  entities %= (result :)
-handle _ = pure ()
+handleNode :: Xeno.Node -> PinboardM ()
+handleNode node = do
+  let nodeName = Text.decodeUtf8 (Xeno.name node)
+  case nodeName of
+    "post" -> do
+      let attrs = Xeno.attributes node -- Now directly ByteString attributes
+      post <- Except.liftEither (createPostFromAttrs attrs)
+      result <- Except.liftEither (postToEntity fromEntityError post)
+      entities %= (result :)
+    _ -> pure ()
+  mapM_ handleContent (Xeno.contents node)
 
-process :: [Tag Text] -> PinboardM Collection
-process tags = do
-  mapM_ handle tags
+handleContent :: Xeno.Content -> PinboardM ()
+handleContent (Xeno.Element node) = handleNode node
+handleContent _ = pure () -- Ignore text and CDATA
+
+processNode :: Xeno.Node -> PinboardM Collection
+processNode rootNode = do
+  handleNode rootNode
   collectedEntities <- use entities
   collection .= Collection.fromEntities collectedEntities
   use collection
 
 parse :: Text -> Either Error Collection
-parse input = do
-  let tags = TagSoup.parseTags input
-  (ret, _) <- runPinboardM (process tags) empty
-  pure ret
+parse input
+  | isNull (Text.strip input) = pure Collection.empty
+  | otherwise = do
+      let inputBS = Text.encodeUtf8 input
+      rootNode <- case Xeno.parse inputBS of
+        Left xenoErr -> Left (XenoError xenoErr)
+        Right node -> Right node
+      (ret, _) <- runPinboardM (processNode rootNode) empty
+      pure ret
