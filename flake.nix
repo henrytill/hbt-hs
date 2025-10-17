3bb55bdfba6a6cb679d08d6b9865a20cae7894fa
Henry Till <henrytill@gmail.com>
2025-10-16 19:09:04 -0700

    flake.nix: enable DWARF support for statically-linked executables

--- flake.nix
+++ flake.nix
@@ -43,7 +43,6 @@
               overrides = hfinal: hprev: {
                 ghc = hprev.ghc.override (
                   prev.lib.optionalAttrs isStatic {
-                    enableDwarf = false;
                     enableNuma = false;
                   }
                 );
@@ -56,21 +55,54 @@
                       path = ./.;
                       name = "hbt-src";
                     }) { };
-                    gmp = prev.gmp.overrideAttrs (_: {
-                      dontDisableStatic = true;
-                    });
-                    libffi = prev.libffi.overrideAttrs (_: {
-                      dontDisableStatic = true;
-                    });
                   in
-                  final.haskell.lib.overrideCabal hbt_ (_: {
-                    configureFlags = final.lib.optionals isStatic [
-                      "--enable-executable-static"
-                      "--extra-lib-dirs=${gmp}/lib"
-                      "--extra-lib-dirs=${libffi}/lib"
-                      "--extra-lib-dirs=${final.zlib.static}/lib"
-                    ];
-                  });
+                  final.haskell.lib.overrideCabal hbt_ (
+                    _:
+                    # Partially adapted from:
+                    # https://sigkill.dk/blog/2024-05-22-static-linking-on-nix-with-ghc96.html
+                    #
+                    # Linking libdw's dependencies by hand allows us to build GHC with DWARF support.
+                    final.lib.optionalAttrs isStatic {
+                      isLibrary = false;
+                      isExecutable = true;
+                      enableSharedExecutables = false;
+                      enableSharedLibraries = false;
+                      configureFlags =
+                        let
+                          bzip2 = prev.bzip2.override {
+                            enableStatic = true;
+                          };
+                          gmp = prev.gmp.overrideAttrs (_: {
+                            dontDisableStatic = true;
+                          });
+                          libffi = prev.libffi.overrideAttrs (_: {
+                            dontDisableStatic = true;
+                          });
+                          xz = prev.xz.override {
+                            enableStatic = true;
+                          };
+                          zlib = final.zlib.static;
+                          zstd = prev.zstd.override {
+                            enableStatic = true;
+                          };
+                        in
+                        [
+                          "--ghc-option=-split-sections"
+                          "--ghc-option=-optl=-static"
+                          "--ghc-option=-optl=-lbz2"
+                          "--ghc-option=-optl=-lelf"
+                          "--ghc-option=-optl=-llzma"
+                          "--ghc-option=-optl=-lz"
+                          "--ghc-option=-optl=-lzstd"
+                          "--extra-lib-dirs=${bzip2.out}/lib"
+                          "--extra-lib-dirs=${gmp}/lib"
+                          "--extra-lib-dirs=${libffi}/lib"
+                          "--extra-lib-dirs=${xz.out}/lib"
+                          "--extra-lib-dirs=${zlib}/lib"
+                          "--extra-lib-dirs=${zstd.out}/lib"
+                        ];
+                    }
+                  );
               };
             };
           };
